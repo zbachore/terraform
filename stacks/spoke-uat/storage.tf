614fdcebe447292c3c0513c77bc1ecfb7f6977b6
zbelachew <zbelachew@gardenranges.com>
2025-12-15 16:13:27 -0500

    Fix spoke storage.tf to use RG data lookup

--- stacks/spoke-uat/storage.tf
+++ stacks/spoke-uat/storage.tf
@@ -1,15 +1,19 @@
 data "azurerm_client_config" "current" {}
 
+data "azurerm_resource_group" "spoke_rg" {
+  name = var.resource_group_name
+}
+
 locals {
   env_code  = "uat"
-  name_hash = substr(md5("${data.azurerm_client_config.current.subscription_id}-${azurerm_resource_group.rg.name}"), 0, 16)
+  name_hash = substr(md5("${data.azurerm_client_config.current.subscription_id}-${data.azurerm_resource_group.spoke_rg.name}"), 0, 16)
   sa_name   = "st${local.env_code}${local.name_hash}"
 }
 
 resource "azurerm_storage_account" "spoke_sa" {
   name                     = local.sa_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.spoke_rg.name
+  location                 = data.azurerm_resource_group.spoke_rg.location
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
